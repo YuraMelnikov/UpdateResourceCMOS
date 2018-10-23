26d10614a5a56a35ca2c50b8819f1afd8264666e
Юрий Мельников <yury.melnikau@gmail.com>
2018-10-23 09:16:56 +0300

    RSAM name update

---
+++ UpdateManpowerKOUsers/Model/ContextProject.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.ProjectServer.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UpdateManpowerKOUsers.Model
+{
+    class ContextProject
+    {
+        private const string pwaPath = "http://tpserver/PWA/";
+        protected static ProjectContext projContext;
+
+        public ContextProject()
+        {
+            projContext = new ProjectContext(pwaPath);
+        }
+    }
+}
---
+++ UpdateManpowerKOUsers/Model/Projects.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using Microsoft.ProjectServer.Client;
+using UpdateManpowerKOUsers.ContextExport1c;
+
+namespace UpdateManpowerKOUsers.Model
+{
+    class Projects : ContextProject
+    {
+        exportImportEntities _db = new exportImportEntities();
+
+
+        public void SetPSAMInProjectName()
+        {
+            projContext.Load(projContext.Projects);
+            projContext.ExecuteQuery();
+            foreach (PublishedProject pubProj in projContext.Projects)
+            {
+                DraftProject projectDraft = pubProj.CheckOut();
+                try
+                {
+                    projectDraft.Name += GetRSAMName(Convert.ToInt32(projectDraft.Name.Substring(0, 4)));
+                }
+                catch 
+                {
+
+                }
+                QueueJob job = projectDraft.Update();
+                job = projectDraft.Publish(true);
+            }
+        }
+
+        private string GetRSAMName(int numberPlanZakaz)
+        {
+            string nameRSAM = "";
+            nameRSAM = _db.planZakaz.First(d => d.Zakaz == numberPlanZakaz.ToString())?.OboznacIzdelia.Replace(" ", "");
+            return nameRSAM;
+        }
+    }
+}
