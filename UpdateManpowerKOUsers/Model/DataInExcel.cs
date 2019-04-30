30faf8ea551f78fbbede5d85c3a99e1b1272a5f0
Юрий Мельников <yury.melnikau@gmail.com>
2019-04-30 16:23:14 +0300

    New alg

--- UpdateManpowerKOUsers/Model/DataInExcel.cs
+++ UpdateManpowerKOUsers/Model/DataInExcel.cs
@@ -20,4 +20,17 @@ namespace UpdateManpowerKOUsers
             Console.WriteLine("{0} | {1} | {2}", Data, Month, UserName);
         }
     }
+
+    class DataExcel
+    {
+        double data;
+        string userName;
+        string month;
+        int numberMonth;
+
+        public string UserName { get => userName; set => userName = value; }
+        public string Month { get => month; set => month = value; }
+        public double Data { get => data; set => data = value; }
+        public int NumberMonth { get => numberMonth; set => numberMonth = value; }
+    }
 }
--- UpdateManpowerKOUsers/Model/ExcelFile.cs
+++ UpdateManpowerKOUsers/Model/ExcelFile.cs
@@ -17,46 +17,98 @@ namespace UpdateManpowerKOUsers.Model
         {
             Excel.Workbook ObjWorkBook = ObjWorkExcel.Workbooks.Open(pathFileRP, 0, true, 5, "", "", false, Excel.XlPlatform.xlWindows, "", true, false, 0, true, false, false);
             Excel.Worksheet ObjWorkSheet = (Excel.Worksheet)ObjWorkBook.Sheets[nameListExcel];
-            
             string textData = "";
             int column = 4;
             int row = 2;
             textData = ObjWorkSheet.Cells[row, column].Text.ToString();
+            DataExcel[] array = GetDefaultDataInExcel(textData);
             while (textData != "")
             {
+                textData = ObjWorkSheet.Cells[row, column].Text.ToString();
+                array = GetDefaultDataInExcel(textData);
                 Console.WriteLine(textData);
                 row += 2;
                 column += 1;
-                while (ObjWorkSheet.Cells[row, column].Text.ToString() == "")
+                for (int i = 0; i < 180; i++)
                 {
-                    row++;
-                    if (row > 1000)
+                    string tmp = ObjWorkSheet.Cells[row + i, column - 1].Text;
+                    for (int j = 0; j < array.Length; j++)
+                    {
+                        if (array[j].Month == ObjWorkSheet.Cells[row + i, column - 1].Text)
+                        {
+                            array[j].Data = Convert.ToDouble(ObjWorkSheet.Cells[row + i, column].Text);
+                        }
+                    }
+                }
+                double startPosition = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    startPosition = array[i].Data;
+                    if (startPosition > 0)
                         break;
                 }
-                for (int i = 0; i < 12; i++)
+                if (startPosition == 0)
+                    startPosition = 0.5;
+                array[0].Data = startPosition;
+                for (int i = 1; i < array.Length; i++)
                 {
-                    try
-                    {
-                        DataInExcel.Add(new DataInExcel(Convert.ToDouble(ObjWorkSheet.Cells[row, column].Text), DateTime.Now.AddMonths(i).Month, textData));
-                        Console.WriteLine();
-                    }
-                    catch
-                    {
-                        row = 13;
-                    }
-                    row++;
+                    if (array[i].Data == 0)
+                        array[i].Data = array[i - 1].Data;
                 }
+                AddDataInExcel(array);
+                PrintUploadResultExcel(array);
                 column += 3;
                 row = 2;
-                try
+            }
+        }
+
+        DataExcel[] GetDefaultDataInExcel(string userName)
+        {
+            DataExcel[] array = new DataExcel[12];
+            int month = DateTime.Now.Month;
+            int year = DateTime.Now.Year;
+            for (int i = 0; i < 12; i++)
+            {
+                array[i] = new DataExcel();
+                array[i].UserName = userName;
+                array[i].Month = year + "." + GeMonthLongString(month);
+                array[i].Data = 0.0;
+                array[i].NumberMonth = month;
+                month++;
+                if (month == 13)
                 {
-                    textData = ObjWorkSheet.Cells[row, column].Text.ToString();
-                }
-                catch
-                {
-                    textData = "";
+                    month = 1;
+                    year++;
                 }
             }
+            return array;
+        }
+
+        string GeMonthLongString(int month)
+        {
+            if (month > 9)
+                return month.ToString();
+            else
+                return "0" + month.ToString();
+        }
+
+        bool PrintUploadResultExcel(DataExcel[] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("{0} | {1} | {2}", array[i].UserName, array[i].Month, array[i].Data);
+            }
+            return true;
+        }
+
+        bool AddDataInExcel(DataExcel[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                DataInExcel dataInExcels = new DataInExcel(array[i].Data, array[i].NumberMonth, array[i].UserName);
+                DataInExcel.Add(dataInExcels);
+            }
+            return true;
         }
     }
 }
