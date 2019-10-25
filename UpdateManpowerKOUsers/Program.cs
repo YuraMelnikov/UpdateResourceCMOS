3e87168750e9c101ebdfdcd6a6bdc81a5cd1f9a9
YuraMelnikov <yury.melnikau@gmail.com>
2019-10-25 10:10:25 +0300

    -

--- UpdateManpowerKOUsers/Program.cs
+++ UpdateManpowerKOUsers/Program.cs
@@ -7,14 +7,14 @@ namespace UpdateManpowerKOUsers
         static void Main(string[] args)
         {
             Projects prj = new Projects();
-            //prj.RenameTasks();
-            prj.SetPSAMInProjectName();
-            ExcelFile excelFile = new ExcelFile();
-            excelFile.UploadDataInListDataInExcel();
-            ProjectResourceManpower projectResourceManpower = new ProjectResourceManpower(excelFile.DataInExcel);
-            projectResourceManpower.UpdateManpower();
-            prj.UpdateCriticalDateClose();
-            NewCurencyBYN curencyBYN = new NewCurencyBYN();
+            prj.RenameTasks();
+            //prj.SetPSAMInProjectName();
+            //ExcelFile excelFile = new ExcelFile();
+            //excelFile.UploadDataInListDataInExcel();
+            //ProjectResourceManpower projectResourceManpower = new ProjectResourceManpower(excelFile.DataInExcel);
+            //projectResourceManpower.UpdateManpower();
+            //prj.UpdateCriticalDateClose();
+            //NewCurencyBYN curencyBYN = new NewCurencyBYN();
         }
     }
 }
